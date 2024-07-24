cee4654fbf7d88b1588ab6bb45050ae3c2cfb88e
Yannick CREPIN <Yannick.crepin@icloud.com>
2024-07-24 15:13:43 +0200

    New message 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
       System.out.println("Hello world!");
-      System.out.println("Yes, I did it");
+      System.out.println("Yes, I did it 2");
     }
 }
